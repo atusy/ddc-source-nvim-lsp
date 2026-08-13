c7be79327d18ec67685ab3f7523d084117256daf
atusy <30277794+atusy@users.noreply.github.com>
2026-08-14 01:58:00 +0900

    test: cover scratch buffer lifecycle

--- tests/test_cmdline_buffers.lua
+++ tests/test_cmdline_buffers.lua
@@ -12,4 +12,26 @@ T["language identities use independent scratch documents"] = function()
   expect.equality(history.uri, "untitled://ddc-cmdline/ddc-history")
 end
 
+T["a wiped scratch document is recreated"] = function()
+  local cmdline = require("ddc_source_nvim_lsp.cmdline")
+  local first = cmdline.ensure_buffer("ddc-wipe")
+  vim.api.nvim_buf_delete(first.bufnr, { force = true })
+  local second = cmdline.ensure_buffer("ddc-wipe")
+  expect.no_equality(first.bufnr, second.bufnr)
+  expect.equality(second.uri, first.uri)
+  expect.equality(vim.api.nvim_buf_is_loaded(second.bufnr), true)
+end
+
+T["an unloaded scratch document is safely reusable"] = function()
+  local cmdline = require("ddc_source_nvim_lsp.cmdline")
+  local first = cmdline.ensure_buffer("ddc-unload")
+  vim.bo[first.bufnr].bufhidden = "hide"
+  vim.api.nvim_buf_delete(first.bufnr, { unload = true })
+  expect.equality(vim.api.nvim_buf_is_valid(first.bufnr), true)
+  expect.equality(vim.api.nvim_buf_is_loaded(first.bufnr), false)
+  local second = cmdline.ensure_buffer("ddc-unload")
+  expect.equality(second, first)
+  expect.equality(vim.api.nvim_buf_is_loaded(second.bufnr), true)
+end
+
 return T
